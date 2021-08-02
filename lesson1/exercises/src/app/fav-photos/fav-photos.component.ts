d51828011f2b6068bba16d3b992a56451e2bfc2c
Sumit Pandey <sumitsenpandey@gmail.com>
2021-08-01 21:21:19 -0500

    Completed Class 18 Exercises: Angular- Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -9,7 +9,7 @@ export class FavPhotosComponent implements OnInit {
   photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
   image2 = 'https://www.nps.gov/common/uploads/grid_builder/jeff/crop16_9/68FEEAFE-D0A3-AE98-C13EFAFAD395D483.jpg?width=950&quality=90&mode=crop';
-  image3 = '';
+  image3 = 'https://cdn.britannica.com/13/146313-050-DD9AAC27/India-War-Memorial-arch-New-Delhi-Sir.jpg';
 
   constructor() { }
 
