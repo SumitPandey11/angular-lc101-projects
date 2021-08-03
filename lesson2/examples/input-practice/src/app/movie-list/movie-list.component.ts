dd94439ef8b9fdbaf2627a0bebed0a91eba88d6b
Sumit Pandey <sumitsenpandey@gmail.com>
2021-08-03 15:19:45 -0500

    completed examples Angular Part 2

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
-
+   errorMessage = false;   
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie(newTitle: string){
+     if(newTitle.trim() != ""){
+      if(!this.movies.includes(newTitle)){
+       this.movies.push(newTitle);
+       this.errorMessage = false;
+      }  else{
+         this.errorMessage = true;
+      } 
+   }
+   else{
+      this.errorMessage = true;
+   }
    }
 }
